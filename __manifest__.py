c3a39da5c51d097e3b4264acf5a2d735b6bb8d6b
jananikangesan <janakanges1996@gmail.com>
2025-06-13 14:15:14 +0530

    customized module

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'ONT INV',
+    'version': '1.0',
+    'category': 'Inventory',
+    'summary': 'Customization for Product Template',
+    'author': 'Janani',
+    'depends': ['base', 'product'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/product_details_views.xml'
+    ],
+    'installable': True,
+    'application': True,
+}
---
+++ models/product_details.py
@@ -0,0 +1,49 @@
+from odoo import models , fields,api
+
+
+class ProductDetails(models.Model):
+    _inherit="product.template"
+
+    ordered_qty=fields.Float(string="Ordered Qty")
+    sold_qty=fields.Float(string="Sold Qty")
+    primary_vendor=fields.Many2one('res.partner', string="Primary Vendor",domain=[('supplier_rank', '>', 0)])
+    mfg_ids=fields.Many2many('product.template',
+        'product_template_mfg_rel',   # Explicit relation table
+        'product_id',                 # Source column
+        'mfg_product_id',              # Target column
+        string="Mfg#",compute='_compute_mfg_ids',
+        store=True)
+    filter_by_product=fields.Many2one("product.template", string="Filter by product")
+    active_status=fields.Selection([("active","Active"),("inactive","Inactive")],string="Active")
+
+    @api.onchange('filter_by_product')
+    def _onchange_filter_by_product(self):
+            product = self.filter_by_product
+            if product:
+                self.name = product.name
+                self.list_price = product.list_price
+                self.standard_price = product.standard_price
+                self.categ_id = product.categ_id.id
+                self.detailed_type = product.detailed_type
+                self.weight = product.weight
+                self.volume = product.volume
+                self.primary_vendor = product.primary_vendor.id
+                self.ordered_qty = product.ordered_qty
+                self.sold_qty = product.sold_qty
+                self.active_status = product.active_status
+                self.mfg_ids = [(6, 0, product.mfg_ids.ids)]
+
+
+    @api.depends('seller_ids.product_code')
+    def _compute_mfg_ids(self):
+        for product in self:
+            codes = product.seller_ids.mapped('product_code')
+            codes = list(filter(None, codes))
+
+            if codes:
+                matching_products = self.env['product.template'].search([
+                    ('default_code', 'in', codes)
+                ])
+                product.mfg_ids = [(6, 0, matching_products.ids)]
+            else:
+                product.mfg_ids = [(6, 0, [])]
