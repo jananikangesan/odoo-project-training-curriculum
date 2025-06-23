13e3c9977cd9ee9e28ee667661de98d1a7522570
jananikangesan <janakanges1996@gmail.com>
2025-06-23 13:39:23 +0530

    file modified

--- __manifest__.py
+++ __manifest__.py
@@ -4,11 +4,13 @@
     'category': 'Inventory',
     'summary': 'Customization for Product Template',
     'author': 'Janani',
-    'depends': ['base', 'product'],
+    'depends': ['base', 'product','purchase'],
     'data': [
         'security/security.xml',
         'security/ir.model.access.csv',
-        'views/product_details_views.xml'
+        'views/product_details_views.xml',
+        'report/purchase_order_report_template.xml'
+
     ],
     'installable': True,
     'application': True,
